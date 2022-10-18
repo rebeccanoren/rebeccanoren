974b1fbddb1313ea3838aa4a9f328bc2e658be21
rebeccanoren <hello@rebeccanoren.com>
2022-10-18 20:46:41 +0200

    updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -43,7 +43,9 @@
 				<figcaption>
 					<p class="uppercase xxs {enter1 ? 'underline' : ''}">TRR (2020)</p>
 					<h3 class="m {enter1 ? 'underline' : ''}">Guiding job-seekers</h3>
-					<span>JTBD</span> <span>User Story Mapping</span> <span>User Test</span>
+					<div class="tags">
+						<span>JTBD</span> <span>User Story Mapping</span> <span>User Test</span>
+					</div>
 				</figcaption>
 			</figure>
 		</a>
@@ -65,7 +67,9 @@
 				<figcaption>
 					<p class="uppercase xxs {enter2 ? 'underline' : ''}">TRR (2019)</p>
 					<h3 class="m {enter2 ? 'underline' : ''}">Matching employers with job-seekers</h3>
-					<span>User Research</span> <span>Impact Mapping</span> <span>Design Sprint</span>
+					<div class="tags">
+						<span>User Research</span> <span>Impact Mapping</span> <span>Design Sprint</span>
+					</div>
 				</figcaption>
 			</figure>
 		</a>
@@ -119,6 +123,7 @@
 
 	img {
 		object-fit: cover;
+		max-width: 100%;
 	}
 
 	.intro {
@@ -137,12 +142,19 @@
 		margin-top: 10px;
 	}
 
+	.tags {
+		display: flex;
+		margin: 6px;
+		gap: 8px;
+	}
+
 	span {
-		background-color: var(--dark);
-		color: var(--pure-white);
+		background-color: #e7f0ff;
+		color: var(--accent-color);
 		padding: 8px 16px;
 		border-radius: 20px;
-		font-size: 12px;
+		font-size: 14px;
+		white-space: nowrap;
 	}
 
 	a:hover {
